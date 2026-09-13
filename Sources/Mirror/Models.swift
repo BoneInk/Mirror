ad7d992d8579afc326c92bc6253102e571fd02fb
boneinkzhong <boneinkzhong@didiglobal.com>
2026-09-14 00:45:13 +0800

    Restore white pages with warm gray workspace

--- Sources/Mirror/Models.swift
+++ Sources/Mirror/Models.swift
@@ -217,7 +217,7 @@ struct EditorTheme: Codable, Hashable, Identifiable, Sendable {
 
     static let paper = EditorTheme(
         id: "builtin.paper", name: "Mirror Light", isDark: false, isBuiltIn: true,
-        backgroundHex: "#FAF8F2", foregroundHex: "#302E2A", mutedHex: "#767066", lineHex: "#D8D2C6",
+        backgroundHex: "#FFFFFF", foregroundHex: "#302E2A", mutedHex: "#767066", lineHex: "#D8D2C6",
         accentHex: "#756951", codeHex: "#F0EDE5", syntaxKeywordHex: "#9A4F5D", syntaxStringHex: "#4E7354",
         syntaxCommentHex: "#636366", syntaxNumberHex: "#9B633C", syntaxTypeHex: "#4F6F7A", syntaxTagHex: "#9A5745"
     )
